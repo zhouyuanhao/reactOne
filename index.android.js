c694fb766af7a94e2c06cf02dd03115f028b60f0
zhouyuanhao <476916922@qq.com>
2016-10-13 22:25:33 +0800

    update library
    update library, now electron and react-native both can run

--- index.android.js
+++ index.android.js
@@ -11,43 +11,14 @@ import {
   Text,
   View
 } from 'react-native';
+import main from './src/main'
 
-class tester extends Component {
+export default class tester extends Component {
   render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
+    return main.call(this, this.props, this.state);
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+
 
 AppRegistry.registerComponent('tester', () => tester);
---
+++ src/main.android.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+export default function () {
+    return (
+      <View>
+        <Text>
+          main.android
+        </Text>
+      </View>
+    );
+}
