abf4754e098d0c9119af988a118d4ee328d6002b
zhouyuanhao <476916922@qq.com>
2016-10-15 18:11:06 +0800

    add

--- src/component/Text.js
+++ src/component/Text.js
@@ -5,7 +5,8 @@ import React, {
 export default class Text extends Component {
 	replaceLineBreak(cont) {
 		if (cont instanceof Array) {
-			return cont.map(v => v == "\n" ? <br key="\n"/> : v);
+			var count = 0;
+			return cont.map(v => v == "\n" ? <br key={'n'+count++}/> : v);
 		}
 		return cont;
 	}
