cc4bfe1751b89d6411603b83e3ff9bf9c7d61dba
zhouyuanhao <476916922@qq.com>
2016-10-18 20:28:15 +0800

    mock window object during test
    mock window object during test

--- __tests__/Util/UtilTest.js
+++ __tests__/Util/UtilTest.js
@@ -1,6 +1,10 @@
+window = {
+	location: {}
+}
 import Util from '../../src/Util/Util';
 
-it('renders correctly', () => {
+
+it('should remove null property in json correctly', () => {
 	var ori = {
 		a: null,
 		b: 1,
@@ -17,4 +21,12 @@ it('renders correctly', () => {
 		}
 	});
 	expect(JSON.stringify(Util.jsonRemoveNull(ori))).toBe(exp);
+});
+
+it('should convert / into root', () => {
+	expect(Util.path2key("/")).toBe("root");
+});
+
+it('should convert /path/file into path_file', () => {
+	expect(Util.path2key("/path/file")).toBe("path_file");
 });
